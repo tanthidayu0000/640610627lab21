55b42250e5a0f687ea238cbfd810875037a2bf6d
thidayu <tanthidayu_0000@hotmail.com>
2022-02-01 02:14:55 +0700

    lab21.2 finished

--- lab21_2.cpp
+++ lab21_2.cpp
@@ -5,8 +5,19 @@ struct Rect{
 	double x,y,w,h;
 };
 
-double overlap(){
-
-
-
+double overlap(Rect *a,Rect *b){
+	double w=0;
+	double h=0;
+	if(a->w + a->x > b->x)
+	{
+		if(a->x + a->w < b->x+b->w)
+			w=a->w - abs(b->x - a->x);
+		else
+			w=b->w;
+		if(a->y + a->h < b->y + b->h)
+			h=b->h - abs(b->y - a->y);
+		else
+			h=b->h;
+	}
+	return w*h;
 }
